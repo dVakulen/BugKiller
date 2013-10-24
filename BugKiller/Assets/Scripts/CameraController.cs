6f65fbcc55fb5be8d0725f75f3474622d99c4f1a
Alex Sikilinda <alex.sikilinda@hotmail.com>
2013-10-24 15:09:32 +0300

    creating project for first iteration. Adding character

---
+++ BugKiller/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = offset + player.transform.position;
+    }
+}
