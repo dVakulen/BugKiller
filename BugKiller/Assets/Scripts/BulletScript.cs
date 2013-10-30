42efcd6758e731642ba2f4cfeffe2dd5695dd46c
Vitaliy <VitalRudeko@gmail.com>
2013-10-30 22:08:58 +0200

    Added shooting, changed gun position, muzzle flashes later

---
+++ BugKiller/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	
+	public float speed = 3f;
+	
+	public GameObject Effect;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("CollilionEnter!");
+		Collisioning();
+	}
+	
+	void OnTriggerEnter()
+	{
+		Debug.Log("TriggerCollisionEnter!");
+		Collisioning();
+	}
+	
+	void Collisioning()
+	{		
+		if(Effect!=null)
+		{
+			Instantiate(Effect,transform.position,Quaternion.identity);	
+		}		
+		Destroy(this.gameObject);
+	}
+}
