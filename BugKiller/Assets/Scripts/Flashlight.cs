21af05c5c4b40e88ad43dd8575ecf478ac311ae2
Dmitry Vakulenko <dimitriu777@gmail.com>
2013-10-31 14:39:14 +0200

    Rewrited code in c#: Dmitry Vakulenko <dimitriu777@gmail.com>

---
+++ BugKiller/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Flashlight : MonoBehaviour {
+	GameObject light;
+	
+	void Start () {
+	 light =GameObject.Find("flashlight");
+	
+	}
+	
+void TurnlightOn(){
+light.light.enabled = false;
+
+}
+
+void TurnlightOff(){
+light.light.enabled = true;
+
+}
+void Update () {
+	if (Input.GetKeyDown(KeyCode.F)){ 
+	if (light.light.enabled == true ){
+		TurnlightOn();
+		} else {
+		TurnlightOff();
+      }
+   }
+	}
+}
