5b962d4948c59fcade5c66f845073ae6cde0e376
Alex Sikilinda <alex.sikilinda@hotmail.com>
2013-11-16 23:43:45 +0200

    renames Bonus.cs -> HPBonus.cs

--- BugKiller/Assets/Scripts/GUI/LevelGUI.cs
+++ BugKiller/Assets/Scripts/GUI/LevelGUI.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class LevelGUI : MonoBehaviour {
+public class LevelGUI : MonoBehaviour
+{
 
     Player player = Player.Instance;
-    
-    void OnGUI() 
+
+    void OnGUI()
     {
         if (player.IsAlive)
         {
-            GUI.Label(new Rect(10, 10, 400, 120), "HP: " + player.Health);    
+            GUI.Label(new Rect(10, 10, 400, 120), "HP: " + player.Health);
         }
         else
         {
-            GUI.Label(new Rect(10, 10, 400, 120), "game over");    
+            GUI.Label(new Rect(10, 10, 400, 120), "game over");
         }
     }
 }
