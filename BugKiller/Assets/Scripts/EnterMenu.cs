aa20751ff4e65bc47b4275474c48ffb3a45b6f8e
Sasha Sokol <sasha.sokolenok@gmail.com>
2013-11-18 13:58:15 +0200

    picture

---
+++ BugKiller/Assets/Scripts/EnterMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterMenu:MonoBehaviour
+{
+	private const int SPLASH = 0;
+	private const int MENU = 1;
+	private int appState;
+
+	void Start()
+	{
+		appState = SPLASH;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown ()) {
+
+				}
+
+				
+	}
+}
+
