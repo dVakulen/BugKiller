55ce0ac1d8a2b767b7dfbc142b423367dbf30239
Alex Sikilinda <alex.sikilinda@hotmail.com>
2013-11-18 14:07:13 +0200

    first sreen changes

--- BugKiller/Assets/Scripts/EnterMenu.cs
+++ BugKiller/Assets/Scripts/EnterMenu.cs
@@ -1,24 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnterMenu:MonoBehaviour
+public class EnterMenu : MonoBehaviour
 {
-	private const int SPLASH = 0;
-	private const int MENU = 1;
-	private int appState;
-
-	void Start()
-	{
-		appState = SPLASH;
-	}
-
 	void Update()
 	{
-		if (Input.GetKeyDown ()) {
-
-				}
-
-				
+		if (Input.anyKey) 
+		{
+			Application.LoadLevel("FirstIterationDemo");
+		}
 	}
 }
 
