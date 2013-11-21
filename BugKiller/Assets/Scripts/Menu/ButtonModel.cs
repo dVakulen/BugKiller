e796e5a0f5730bc3df96d0ec4d5e0f77f510c3b8
Vitaliy <VitalRudeko@gmail.com>
2013-11-21 17:26:07 +0200

    First part of menus

---
+++ BugKiller/Assets/Scripts/Menu/ButtonModel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ButtonModel : MonoBehaviour {
+
+	// Use this for initialization
+	Color start;
+	public Color hover;
+
+	void Start () {
+		start = transform.GetComponent<TextMesh>().color;
+		hover = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseEnter()
+	{
+		transform.GetComponent<TextMesh>().color = hover;
+	}
+
+	void OnMouseExit() 
+	{
+		transform.GetComponent<TextMesh>().color = start;
+	}
+}
