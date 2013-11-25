0ffd45090b0fac6cce8b5442c4847999f4f9db38
Alex Sikilinda <alex.sikilinda@hotmail.com>
2013-11-25 13:56:38 +0200

    some changes in level1

---
+++ BugKiller/Assets/Scripts/LevelCompleter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCompleter : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			Application.LoadLevel("MainScreen");
+		}
+	}
+}
