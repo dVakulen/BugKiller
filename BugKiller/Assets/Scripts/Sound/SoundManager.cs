dfa00a26f4bd6707819e26324c38d1d21e848700
Dmitry Vakulenko <dimitriu777@gmail.com>
2013-11-25 14:40:54 +0200

    Refactoring

---
+++ BugKiller/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+		public AudioClip[] bugFootstepSounds;
+		public  AudioClip[] playerScreams;
+		public AudioClip[] playerFootstepSounds  ;
+		public AudioClip[] playerHitted  ;
+		public static AudioClip[] playerHitted1  ;
+		public static AudioClip[] playerFootstepSounds1;
+		public static AudioClip[] playerScreams1;
+		public static AudioClip[] bugFootstepSounds1;
+
+		public enum Soundtype
+		{
+				Player,
+				Bug,
+				Shot
+		}
+
+
+		void Awake ()
+		{
+				playerHitted1 = playerHitted;
+				playerScreams1 = playerScreams;
+				bugFootstepSounds1 = bugFootstepSounds;
+				playerFootstepSounds1 = playerFootstepSounds;
+
+		}
+
+		public 	 static AudioClip GetPlayerFootstepSound ()
+		{
+				return GetRandomSoundFromArray (playerFootstepSounds1);
+		}
+
+		public 	 static AudioClip GetPlayerHitted ()
+		{
+				return GetRandomSoundFromArray (playerHitted1);
+		}
+
+		public 	 static AudioClip GetBugFootstepSounds ()
+		{
+				return GetRandomSoundFromArray (bugFootstepSounds1);
+		}
+
+		public 	 static AudioClip GetPlayerScreams ()
+		{
+				return GetRandomSoundFromArray (playerScreams1);
+		}
+
+		static     AudioClip GetRandomSoundFromArray (AudioClip[] audioClipArray)
+		{
+
+				if (audioClipArray.Length > 0)
+						return  audioClipArray [Random.Range (0, audioClipArray.Length)];
+				return null;
+		}
+
+}
