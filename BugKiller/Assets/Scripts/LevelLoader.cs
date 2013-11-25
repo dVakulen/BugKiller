4fb804d5580117ee8df9eeed967bb5cca4de6115
Alex Sikilinda <alex.sikilinda@hotmail.com>
2013-11-25 23:39:27 +0200

    adds posibility to load level 1

---
+++ BugKiller/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public string LevelName;
+
+	void OnTriggerStay(Collider other) 
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			if (Input.GetKeyDown(KeyCode.E)) 
+			{
+				Application.LoadLevel(LevelName);
+			}
+		}
+	}
+}
