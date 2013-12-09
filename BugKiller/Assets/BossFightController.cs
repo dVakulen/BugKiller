62212899728ad92497d73e7982b82a40baa31f9a
Dmitry Vakulenko <dimitriu777@gmail.com>
2013-12-10 01:23:21 +0200

    Camera pos

--- BugKiller/Assets/BossFightController.cs
+++ BugKiller/Assets/BossFightController.cs
@@ -11,26 +11,25 @@ public class BossFightController : MonoBehaviour
 		{
 				boss = GameObject.Find ("BOSS");
 				boss.SetActive (false);
-		player = GameObject.FindGameObjectWithTag ("Player").transform;
+				player = GameObject.FindGameObjectWithTag ("Player").transform;
 
-		if (WeaponManager.levelcompleted == 1)
-		{
-			GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
-			player.position = GameObject.Find("LevelLoader1").transform.position;
-			cam.transform.position = cam.transform.position + new Vector3(3f,0,0);
-		}
-
+				if (WeaponManager.levelcompleted == 1) {
+						GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+						player.position = GameObject.Find ("LevelLoader1").transform.position;
+						cam.transform.position = cam.transform.position + new Vector3 (5f, 0, 0);
+				}
 
 
 
-				if (WeaponManager.weaponsCount >=3) {
+
+				if (WeaponManager.weaponsCount >= 3) {
 			
-			Debug.LogError(WeaponManager.weaponsCount.ToString());
+						Debug.LogError (WeaponManager.weaponsCount.ToString ());
 						
 						player.position = Point.position;
 						boss.SetActive (true);
-			GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
-			cam.transform.position = cam.transform.position + new Vector3(0,2.4f,0); 
+						GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
+						cam.transform.position = cam.transform.position + new Vector3 (0, 2.4f, 0); 
 				}
 		}
 	
