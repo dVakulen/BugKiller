7c9efd685e23b1d98cc82279ff8f33846fa91dd1
Roman <sigma-project@hotmail.com>
2013-12-10 11:50:31 +0200

    #52 done, code refactoring

---
+++ BugKiller/Assets/Scripts/GameLogicReseter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogicReseter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WeaponManager.Reset();
+	}
+}
--- BugKiller/Assets/Scripts/Menu/NewGameBehavior.cs
+++ BugKiller/Assets/Scripts/Menu/NewGameBehavior.cs
@@ -2,17 +2,12 @@
 using System.Collections;
 
 public class NewGameBehavior : MonoBehaviour {
-
 	public string StartLevel = "GenderSelectScreen";
+
 	// Use this for initialization
 	void Start () {
 		Screen.showCursor = true;
 		Screen.lockCursor = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	void OnMouseDown()
